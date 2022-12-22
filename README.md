3d6eff49248720579b36b0f3813d01429de11075
Breno Freitas da Silva <freitasbreno174@gmail.com>
2022-12-22 00:40:43 -0300

    01

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Nodets
+
+### Pré-requisistos globais:
+` npm i -g nodemon typescript ts-node`
+
+### Instalação
+`npm install`
+
+### Para rodar o projeto
+`npm run start`
---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import mustache from 'mustache-express';
+import path from 'path';
+import mainRouter from './routes/index';
+
+dotenv.config();
+
+const server = express();
+
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
+
+server.use(express.static(path.join(__dirname, '../public')));
+
+server.use(mainRouter);
+
+server.use((req, res)=>{
+    res.send("pagina não encontrada");
+})
+
+
+server.listen(process.env.PORT);
+
